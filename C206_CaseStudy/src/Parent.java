1cf8ecb06d095ad3e5cda050426dc2f1f98548c9
Alfian <Scrip@DESKTOP-MC2VKDF>
2020-08-20 16:43:54 +0800

    I hate this (Completed add,view,delete and menu)

--- C206_CaseStudy/src/Parent.java
+++ C206_CaseStudy/src/Parent.java
@@ -1,26 +1,80 @@
 
 public class Parent {
-	private String parentid;
-	private String parent;
+	private String name;
+	private String email;
+	private String studentid;
+	private String studentName;
+	private String studentGrade;
+	private String classroom;
+	private String teacher;
 	
-	public Parent(String parentid, String parent) {
-		this.parentid = parentid;
-		this.parent = parent;
-	}
-	
-	public String getparentid() {
-		return parentid;
-	}
-	
-	public void setparentid(String parentid) {
-		this.parentid = parentid;
-	}
-	public String getparent() {
-		return parent;
-	}
-	public void setparent(String parent) {
-		this.parent = parent;
-		
+	public Parent(String name, String email, String studentid, String studentName, String studentGrade, String classroom, String teacher) {
+		this.name = name;
+		this.email = email;
+		this.studentid = studentid;
+		this.studentName = studentName;
+		this.studentGrade = studentGrade;
+		this.classroom = classroom;
+		this.teacher = teacher;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getStudentid() {
+		return studentid;
+	}
+
+	public void setStudentid(String studentid) {
+		this.studentid = studentid;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getStudentGrade() {
+		return studentGrade;
+	}
+
+	public void setStudentGrade(String studentGrade) {
+		this.studentGrade = studentGrade;
+	}
+
+	public String getClassroom() {
+		return classroom;
+	}
+
+	public void setClassroom(String classroom) {
+		this.classroom = classroom;
+	}
+
+	public String getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+	 public String toString(){
+		  return name + " " + email + " " + studentid + " " + studentName + " " + studentGrade + " " + classroom + " " + teacher; 
+
+}
 }
